a61070a7ad7d7d4c09ab2d9cab73bdea26e2ec82
ArisaBonsaiTree <angelevarela@hotmail.com>
2021-11-05 01:21:05 -0700

    Finished

--- shell.java
+++ shell.java
@@ -1,130 +1,170 @@
 import java.io.*;
 import java.util.Arrays;
 
-// TODO: Perhaps prevent broken directories
-// TODO: Refactor
-// TODO: ..   ../..  ../../..
-// TODO BUG   cd /arisa --> sends you up a directory
-
 /* HINT GIVEN TO US
     // check if we are going to an absolute or relative directory
     if (!newDirectory.startsWith(fileSeparator))  // relative path
     newDirectory = workingDirectory + newDirectory;
  */
-public class ImprovedTerminal extends Thread{
+
+public class shell extends Thread{
 
     private BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
     private ProcessBuilder processBuilder = new ProcessBuilder();
     private Process process;
     private StringBuilder builder = new StringBuilder();
     private int dotCounter = 0;
+    private String[] result;
+    String storePath = "/home";
 
-
-    // I don't like working in main, so I made an object to run a method that isn't static
     public static void main(String[] args) throws IOException, InterruptedException {
-        new ImprovedTerminal().runMe();
+        new shell().runMe();
     }
 
-
     private void runMe() throws InterruptedException, IOException {
+        processBuilder.directory(new File("/home"));
+
         while (true){
             try {
                 System.out.print("$> ");
 
                 // "cd .." --> cd[0] ..[1] :: ONLY SPACES
-                String[] result = console.readLine().split("\\s");
+                result = console.readLine().split("\\s");
 
-                // Inputting \n or Enter will just send you back
-                if (result[0].equals("")) {
-                    continue;
-                }
+                if (result[0].equals("")) continue;
 
                 // Commands are loaded into processBuilder
                 processBuilder.command(result);
 
                 // Close the application if 'exit' was typed
-                if(result[0].equalsIgnoreCase("exit"))
-                    System.exit(0);
+                if(result[0].equalsIgnoreCase("exit")) System.exit(0);
 
+
+                // ------------------------------------------------------------
 
                 // If the user types cd --> Some logic we go through
                 if(result[0].startsWith("cd")){
 
-                    // cd ./ -- Relaltive path?
-                    if(result[1].startsWith(".") && result[1].startsWith("/", 1)){
-
-                        String rudendent = convertArrayToString(result);
-
-
-                        String[] breakUpDir = rudendent.split("/");
+                    // Relative path => cd ./
+                    // Suppose we are typing cd ./arisa   pwd: /home
+                    if (result[1].startsWith(".") && result[1].startsWith("/", 1)) {
+                        String cantSplitAnArray = convertArrayToString(result); // REMOVES CD => ./arisa
+                        String[] breakUpStringResult = cantSplitAnArray.split("/"); // .[0] arisa[1]
 
                         String[] dir = getDirectory().split("/");
 
-                        for (int i = 0; i < dir.length; i++) {
+                        // Adds the current path into the builder
+                        for (int i = 1; i < dir.length; i++) { // Add one, since a blank is counted
                             builder.append("/").append(dir[i]);
                         }
 
-                        for (int i = 0; i < breakUpDir.length; i++) {
-                            builder.append("/").append(breakUpDir[i]);
+                        // Appends the rest
+                        for (int i = 1; i < breakUpStringResult.length; i++) {
+                            builder.append("/").append(breakUpStringResult[i]);
                         }
 
+                        // Store the path before replacing it
+                        storePath = processBuilder.directory().getAbsolutePath();
                         processBuilder.directory(new File(builder.toString()));
-                        builder.setLength(0); // Reset string builder
 
-                        continue;
+                        if(validateDirectoy(builder.toString())){
+                            // It's a real directory,
+                            builder.setLength(0); // Reset builder
+                            continue;
+                        }else {
+                            // Not a real directory it must be resetted
+                            processBuilder.directory(new File(storePath));
+                            builder.setLength(0); // Reset builder
+                            continue;
+                        }
                     }
 
                     // Go up a directory :: Looking for cd ..
-                    if(result[1].startsWith(".") && result[1].startsWith(".",1)){
+                    if (result[1].startsWith(".") && result[1].startsWith(".", 1)) {
                         goUpADirectory(result, true);
                         continue;
                     }
+                    
+                    // IF you type cd arisa
+                    if(!result[1].startsWith("/")){
+                        builder.append(getDirectory()).append("/").append(result[1]);
 
-
-                    // Absolute path
-                    for(int i = 1; i < result.length; i++){
-                        // cd /home/arisa/Desktop/Folder
-                        builder.append("/").append(result[i]);
+                        storePath = processBuilder.directory().getAbsolutePath();
                         processBuilder.directory(new File(builder.toString()));
-                        builder.setLength(0); // Reset string builder
+
+                        if(validateDirectoy(builder.toString())){
+                            // It's a real directory,
+                            builder.setLength(0); // Reset builder
+                            continue;
+                        }else {
+                            // Reset it
+                            processBuilder.directory(new File(storePath));
+                            builder.setLength(0); // Reset builder
+                            continue;
+                        }
+
                     }
 
 
-                    //processBuilder.directory(new File("/home"));
-                    continue;
+                    // typing /home/arisa/Desktop ========
+                    // Store the path
+                    storePath = processBuilder.directory().getAbsolutePath();
+                    // Place what ever you have after cd
+                    processBuilder.directory(new File(result[1]));
+
+                    // Validates to see if it works
+                    if(validateDirectoy(result[1])){ // Absolute path  cd home/arisa/Desktop
+                        // It's a real directory,
+                        builder.setLength(0); // Reset builder Just in case
+                        continue;
+                    }
+                    else {
+                        // Not a real directory it must be resetted
+
+                        processBuilder.directory(new File(storePath));
+                        builder.setLength(0); // Reset builder
+                        continue;
+                    }
+                } // end of cd [logic]
+
+                else {
+                    process = processBuilder.start();
+                    printStream();
                 }
-                // BREAK THIS UP
-                // ====================================================
-
-                // Activate process Builder
-                process = processBuilder.start();
-
-                // Runs nearly everything EXCEPT cd..
-                printStream();
             }
-            // TODO: Fix error handling to specific errors
-            catch (IOException e) { // Just catch anything
-                processBuilder.directory(new File("/home"));
+            catch (Exception e) { // Just catch anything   NOT FOR CD
+                System.out.println(result[0] + ": command not found");
+                processBuilder.directory(new File(storePath));
                 continue;
             }
         }
     }
 
-    // Some magic to allow us to splice things + removed brackets and such
+
+
+
+    // Will try to look for a directory with the given content :: Throw an error if it can't do so
+    private boolean validateDirectoy(String directoryPath){
+        try{
+            getDirectory();
+            return true;
+        } catch (IOException e) {
+            System.out.println("bash: cd: " + result[1] + ": No such file or directory found");
+            return false;
+        }
+    }
+
     private String convertArrayToString(String[] convertMe){
         // Remove the cd from the array, 'purifies it'
         String[] removeTheCD = Arrays.copyOfRange(convertMe, 1, convertMe.length);
-
         // Now turn convert an array into a string, HOWEVER NOT IT HAS BRACKETS
         String convertArrayToString = Arrays.toString(removeTheCD);
-
         // Return the String, but remove the brackets, move it up by one and lower it by one first --> [....] <--last
         return convertArrayToString.substring(1, convertArrayToString.length() - 1);
     }
 
-
     private void goUpADirectory(String[] findDots, boolean shouldCount) throws IOException {
-        // We only want this to run one time to find the amount of ".."
+        // We only want this to one once, otherwise skip this
         if(shouldCount == true) {
             String dotFinder = convertArrayToString(findDots);
             String[] dotBreaker = dotFinder.split("/");
@@ -139,34 +179,38 @@ public class ImprovedTerminal extends Thread{
         // Break the directories up "/home/arisa/Desktop" --> [0]home, [1]arisa, [2]Desktop
         String[] dir = getDirectory().split("/");
 
-        // '/ + [i]' + '/ + [i]' + ...
-        for (int i = 0; i < dir.length - 1; i++) {
-            builder.append("/").append(dir[i]);
+        if(dir.length == 0){
+            // Prevents it from going any lower than '/'
+            dotCounter = 0;
         }
+        else {
 
-        processBuilder.directory(new File(builder.toString()));
-        builder.setLength(0); // Reset string builder
-        dotCounter--;
+            // '/ + [i]' + '/ + [i]' + ...   :: We reduce the length by one! Removing the last directory!
+            for (int i = 0; i < dir.length - 1; i++) {
+                builder.append("/").append(dir[i]);
+            }
+
+            processBuilder.directory(new File(builder.toString()));
+
+            builder.setLength(0); // Reset string builder
+            dotCounter--;
+        }
         if(dotCounter > 0){
             String[] dummy = null;
             goUpADirectory(dummy, false);
         }
-
     }
 
-
-    public void printStream() throws IOException {
-        // Step 4) Output Stream
+    private void printStream() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         String line;
 
         while ((line = reader.readLine()) != null) {
-            // Step 5) Output the contents
             System.out.println(line);
         }
     }
 
-    public String getDirectory() throws IOException {
+    private String getDirectory() throws IOException {
         // Load the command "pwd" into processBuilder to make it Print Working Directory
         processBuilder.command("pwd");
 
